35bc279a9a8ccb1d302ad3e958ed4306385ba412
Dair Aban <2016114132@ub.edu.bz>
2025-05-04 13:17:48 -0600

    Implemented broadcasting feature

--- tcp/server/handler.go
+++ tcp/server/handler.go
@@ -2,25 +2,53 @@
 package server
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"sync"
+)
+
+var (
+	clients = make(map[net.Conn]string)
+	mu      sync.Mutex
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024)
+	addr := conn.RemoteAddr().String()
 
+	// Register client
+	mu.Lock()
+	clients[conn] = addr
+	mu.Unlock()
+
+	fmt.Printf("[+] %s connected\n", addr)
+
+	reader := bufio.NewReader(conn)
 	for {
-		n, err := conn.Read(buf)
+		message, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Error reading from client:", err)
+			fmt.Printf("[-] %s disconnected\n", addr)
+			mu.Lock()
+			delete(clients, conn)
+			mu.Unlock()
 			return
 		}
 
-		// Echo message back to client
-		_, err = conn.Write(buf[:n])
-		if err != nil {
-			fmt.Println("Error writing to client:", err)
+		broadcast := fmt.Sprintf("[%s]: %s", addr, message)
+		broadcastMessage(conn, broadcast)
+	}
+}
+
+func broadcastMessage(sender net.Conn, message string) {
+	mu.Lock()
+	defer mu.Unlock()
+	for client := range clients {
+		if client != sender {
+			_, err := client.Write([]byte(message))
+			if err != nil {
+				fmt.Println("Error broadcasting:", err)
+			}
 		}
 	}
 }
